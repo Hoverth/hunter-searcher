3ae13934e8e58442c836b3e1cad50c9ca4112ac0
Thomas Dickson <modscrat+github@gmail.com>
2024-05-22 16:12:23 +1200

    doc: documenting index.rs

--- src/index.rs
+++ src/index.rs
@@ -1,17 +1,25 @@
 use std::collections::HashMap;
 
+/// Index Entry - A index entry format struct
 #[derive(Clone,Debug)]
 pub struct IndexEntry {
-    pub url: String,
-    pub number_js: usize,
-    pub links: Vec<String>,
-    pub tf: HashMap<String, usize>,
+    /// The URL of the document
+    pub url: String, 
+    /// The number of Javascript scripts in the document
+    pub number_js: usize, 
+    /// The links in the document
+    pub links: Vec<String>, 
+    /// The term frequency table for the document
+    pub tf: HashMap<String, usize>, 
+    /// The text content of the document
+    pub content: String, 
+    /// A small blurb for the document
+    pub blurb: String, 
 }
 
-pub fn process_index(index: Vec<IndexEntry>) {
+pub async fn process_index(index: Vec<IndexEntry>) {
     for i in index {
         println!("{i:?}");
     }
-
-    // alright, postgresql with a relational db of tokens <-> documents i guess
 }
+
