74f3b89fa57afe2ec2875c595cd8c04fa49cf915
Thomas Dickson <modscrat+github@gmail.com>
2024-05-24 15:40:56 +1200

    feat: added command & argument parsing with clap.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use log::{debug, info};
 use std::env;
+use clap::Parser;
 
 mod crawler;
 mod db;
@@ -9,9 +10,25 @@ use crate::crawler::CrawlerBuilder;
 use crate::app::serve;
 use crate::db::DB;
 
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = "A simple search engine in rust")]
+struct Args {
+    #[arg(index = 1, help = "either 'serve' or 'crawl'")]
+    command: String,
+
+    #[arg(long,short,default_value_t=String::new(),help="the url to crawl")]
+    url: String,
+
+    #[arg(long,short,default_value_t=1,help="the max amount of pages to crawl (set to -1 to infinitely crawl)")]
+    depth: i32,
+}
+
 #[tokio::main]
 async fn main() {
     env_logger::init();
+
+    let args = Args::parse();
+    println!("{args:?}");
     
     let db = DB::new(
         &env::var("POSTGRES_USER").expect("No POSTGRES_USER env var set!"), 
@@ -20,15 +37,23 @@ async fn main() {
         &env::var("POSTGRES_DB").expect("No POSTGRES_DB env var set!")
         ).await;
 
-    info!("Started crawler!");
+    if args.command == "crawl" {
+        if args.url == "" { panic!("Cannot crawl a blank URL!"); }
 
-    let mut crawler = CrawlerBuilder::new("hunter-searcher crawler/v0.1.0")
-                                    .max_depth(5)
-                                    .build();
-    
-    debug!("Created Crawler from builder");
+        let mut url = args.url;
+        if !url.starts_with("http") { url = "http://".to_owned() + &url }
+        info!("Started crawler!");
+        
+        let mut crawler = CrawlerBuilder::new("hunter-searcher crawler/v0.1.0")
+                                        .max_depth(args.depth)
+                                        .build();
+        
+        debug!("Created Crawler from builder");
 
-    let _ = crawler.crawl(&db, "https://example.com").await;
+        let _ = crawler.crawl(&db, &url).await;
+    }
 
-    serve(db).await;
+    if args.command == "serve" {
+        serve(db).await;
+    }
 }
